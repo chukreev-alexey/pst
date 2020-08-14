19d949c6d5fa9860692c01e29cbcdb389a38e2f8
Arkadiy Zamaraev <arkadiy@bk.ru>
2020-08-14 18:01:28 +0500

    update page

--- static/js/__build/__main.js
+++ static/js/__build/__main.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "75b3b6226b37aab03a36";
+/******/ 	var hotCurrentHash = "ff3236c8cb33c07002c0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
