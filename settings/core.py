1432b6984299bc36c4158c755fa63705c78b81d4
Andrey <whatzalogin@gmail.com>
2020-08-27 17:54:26 +0500

    issue#87. add sitemaps for itcase_pages, itcase_feed, catalog.categories and catalog.products

--- settings/core.py
+++ settings/core.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'django.contrib.messages',
     'django.contrib.sessions',
+    'django.contrib.sitemaps',
     'django.contrib.sites',
     'django.contrib.staticfiles',
 
