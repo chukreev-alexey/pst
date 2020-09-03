9a12d7aadcf3c2e6d03bee14ea4b02f46dc6bdca
Nikita <ggift@mail.ru>
2020-09-03 20:04:57 +0500

    new search initialization

--- static/js/main.js
+++ static/js/main.js
@@ -3,12 +3,17 @@
 'use strict'
 
 if (typeof SearchClass !== 'undefined') {
-  global.ItcaseSearch = new SearchClass({
-    quickSearchOpenClass: 'search-input',
-    quickSearchPopupClass: 'search_type_quick',
-    quickSearchVisibleClass: 'search_type_quick_state_visible',
-    quickSearchHideClass: 'search_type_quick_state_hidden',
-    clearButtonClass: 'search-input__clear',
+  const searchInputs = document.querySelectorAll('input[name="search_input_text"]')
+  searchInputs.forEach((input) => {
+    // global.ItcaseSearch = new SearchClass({
+    new SearchClass({
+      element: input,
+      quickSearchOnOpenClass: 'search-input',
+      quickSearchPopupClass: 'search_type_quick',
+      quickSearchVisibleClass: 'search_type_quick_state_visible',
+      quickSearchHiddenClass: 'search_type_quick_state_hidden',
+      clearValueButtonClass: 'search-input__clear',
+    })
   })
 }
 
