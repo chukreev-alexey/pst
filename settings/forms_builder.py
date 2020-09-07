7a698c093a73732e091542f118ef4166b8dc22db
Arkadiy Zamaraev <arkadiy@bk.ru>
2020-09-07 17:15:21 +0500

    update form

--- settings/forms_builder.py
+++ settings/forms_builder.py
@@ -10,4 +10,6 @@ FORMS_BUILDER_UPLOAD_ROOT = os.path.join(MEDIA_ROOT, FILEBROWSER_DIRECTORY)
 
 FORMS_BUILDER_EXTRA_FIELDS = (
     (100, 'captcha.fields.ReCaptchaField', 'ReCaptcha'),
+    (101, 'itcase_common.fields.AgreementProcessingPDField',
+     'Соглашение на обработку ПД'),
 )
