f28fe568d08e64c894f15197ce41f4f03f56e77c
Arkadiy Zamaraev <arkadiy@bk.ru>
2020-09-14 13:19:18 +0500

    add global message

--- settings/grappelli.py
+++ settings/grappelli.py
@@ -21,7 +21,7 @@ See for more details: https://github.com/sehmaschine/django-grappelli.
 """
 
 # The Site Title of your admin interface.
-GRAPPELLI_ADMIN_TITLE = 'pst'
+GRAPPELLI_ADMIN_TITLE = 'Профстройторг'
 
 # A dictionary containing search patterns for models you cannot (or should not)
 # alter.
